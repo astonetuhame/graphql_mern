13ce9886a9bd7c897af854b578f240a7f8dbb7dc
astonetuhame <astonetuhame@gmail.com>
2022-06-06 23:15:37 +0300

    Delete project mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -128,6 +128,15 @@ const mutation = new GraphQLObjectType({
                 })
                 return project.save()
             }
+        },
+        deleteProject: {
+            type: ProjectType,
+            args: {
+                id: {type: GraphQLNonNull(GraphQLID)}
+            },
+            resolve(parent, args){
+                return Project.findByIdAndRemove(args.id)
+            }
         }
     }
 })
