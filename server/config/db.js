9d6e44ec6fd8c7e52e0ea1cbdd735b8cc72313cc
astonetuhame <astonetuhame@gmail.com>
2022-06-06 22:32:15 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,5 +4,7 @@ const connectDB = async () => {
 
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB Connected: ${conn.connection,host}`.cyan.underline.bold)
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
 }
+
+module.exports = connectDB 
