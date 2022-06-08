c344cef3b6701e87e0e9d25be1244e433a3525ea
astonetuhame <astonetuhame@gmail.com>
2022-06-08 21:31:21 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,48 @@
+import Header from "./components/Header";
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
+import  Clients from './components/Clients'
+import  Projects from './components/Projects'
+import AddClientModal from "./components/AddClientModal";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  url: 'http://localhost:5000/graphql',
+  cache,
+})
+
 function App() {
   return (
-    <div className="container">
-      <h1>Hello World</h1>
-     
+    <>
+    <ApolloProvider client={client}>
+    <Header />
+     <div className="container">
+       <AddClientModal />
+       <Projects />
+      <Clients />
     </div>
+    </ApolloProvider>
+    </>
   );
+
+   
 }
 
 export default App;
