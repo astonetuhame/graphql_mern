45fe967cedc20b0c6891c865328b32ef41151c33
astonetuhame <astonetuhame@gmail.com>
2022-06-08 21:31:27 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const colors = require('colors')
+const cors = require('cors')
 require('dotenv').config();
 
 const { graphqlHTTP } = require('express-graphql')
@@ -12,6 +13,7 @@ connectDB()
 
 const app = express();
 
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development'
