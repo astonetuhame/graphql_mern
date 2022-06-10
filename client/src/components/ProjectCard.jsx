fa28a6a29dd706a6c6f5a6a58f2c0b78a7ba57ca
astonetuhame <astonetuhame@gmail.com>
2022-06-10 17:30:29 +0300

    Update ProjectCard.jsx

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,5 +1,5 @@
 
-export default function ProjectCard() {
+export default function ProjectCard({project}) {
   return (
     <div className="col-md-6">
         <div className="card mb-3">
@@ -8,7 +8,7 @@ export default function ProjectCard() {
                     <h5 className="card-title">
                         {project.name}
                     </h5>
-                    <a className='btn btn-light' href={`/projects/{$project.id}`}>
+                    <a className='btn btn-light' href={`/projects/${project.id}`}>
                          View
                     </a>
                 </div>
