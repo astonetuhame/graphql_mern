6db00e7bfd1bb2d44cb8353640e39697cf329ad1
astonetuhame <astonetuhame@gmail.com>
2022-06-10 16:07:06 +0300

    Create Home.jsx

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import AddClientModal from '../components/AddClientModal'
+import AddProjectModal from '../components/AddProjectModal'
+import Clients from '../components/Clients'
+import Projects from '../components/Projects'
+
+
+export default function Home() {
+  return (
+    <>
+    <div className="d-flex gap-3 mb-3">
+    <AddClientModal />
+    <AddProjectModal />
+    </div>
+    <Projects />
+    <hr />
+    <Clients />
+    </>
+  )
+}
